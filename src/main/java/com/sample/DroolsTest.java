9784e65ad9c52d4ceff89bf9000d87a144931288
Jakub Woźniak <mescam@mescam.pl>
2015-01-10 20:21:53 +0100

    Answers

--- src/main/java/com/sample/DroolsTest.java
+++ src/main/java/com/sample/DroolsTest.java
@@ -34,6 +34,7 @@ public class DroolsTest {
     private static KnowledgeBase readKnowledgeBase() throws Exception {
         KnowledgeBuilder kbuilder = KnowledgeBuilderFactory.newKnowledgeBuilder();
         kbuilder.add(ResourceFactory.newClassPathResource("Questions.drl"), ResourceType.DRL);
+        kbuilder.add(ResourceFactory.newClassPathResource("Knowledge.drl"), ResourceType.DRL);
         KnowledgeBuilderErrors errors = kbuilder.getErrors();
         if (errors.size() > 0) {
             for (KnowledgeBuilderError error: errors) {
@@ -46,31 +47,6 @@ public class DroolsTest {
         return kbase;
     }
 
-    public static class Message {
-
-        public static final int HELLO = 0;
-        public static final int GOODBYE = 1;
-
-        private String message;
-
-        private int status;
-
-        public String getMessage() {
-            return this.message;
-        }
-
-        public void setMessage(String message) {
-            this.message = message;
-        }
-
-        public int getStatus() {
-            return this.status;
-        }
-
-        public void setStatus(int status) {
-            this.status = status;
-        }
-
-    }
+   
 
 }
---
+++ src/main/java/pl/poznan/put/fc/Answer.java
@@ -0,0 +1,9 @@
+package pl.poznan.put.fc;
+
+import javax.swing.JOptionPane;
+
+public class Answer {
+	public Answer(String answer) {
+		JOptionPane.showMessageDialog(null, answer);//, "Car Mechanic Answer", JOptionPane.OK_OPTION);
+	}
+}
